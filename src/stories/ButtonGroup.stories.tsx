e68980562c492c2bf89fa6efed94852082a34b61
Sengolda <79252176+Sengolda@users.noreply.github.com>
2022-03-20 17:57:44 +0530

    Add TS stuff

---
+++ src/stories/ButtonGroup.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Button from "../components/Button";
+import ButtonGroup from "../components/ButtonGroup";
+
+export default {
+  title: 'ButtonGroup',
+  component: ButtonGroup,
+} as ComponentMeta<typeof ButtonGroup>;
+
+const Template: ComponentStory<typeof ButtonGroup> = ({ children, ...args }) => (
+    <ButtonGroup {...args}>{children}</ButtonGroup>
+);
+
+export const ButtonGroupStory = Template.bind({})
+
+ButtonGroupStory.args = {
+    children: (
+      <>
+        <Button color="primary">First</Button>
+        <Button color="primary">Middle</Button>
+        <Button color="primary">Last</Button>
+      </>
+    ),
+    gap: '0.5rem'
+}
