08e8db9b1a5d7ee6ee9e772532471ef3f7dc0e43
Sengolda <79252176+Sengolda@users.noreply.github.com>
2022-03-20 16:35:55 +0530

    Co-authored-by: ibraim <ibraim6@users.noreply.github.com>

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -30,38 +30,7 @@ const Button: FC<PropsWithChildren<ButtonProps>> = ({ children, color }) => {
         {children}
       </button>
     );
-  } else if (color === "success")
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-success"]}`}
-      >
-        {children}
-      </button>
-    );
-  else if (color === "error")
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-danger"]}`}
-      >
-        {children}
-      </button>
-    );
-  else if (color === "info")
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-info"]}`}
-      >
-        {children}
-      </button>
-    );
-  else if (color === "warning")
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-warning"]}`}
-      >
-        {children}
-      </button>
-    );
+    }
   // we should add an else here which returns theme provided button
 };
 
