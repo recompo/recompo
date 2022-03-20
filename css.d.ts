39536c6cc990bd80864181856740763de7e0edcf
Gurpal Singh <gillgurpal699@gmail.com>
2022-03-20 16:30:06 +0530

    add type declarations for css files

--- css.d.ts
+++ css.d.ts
@@ -4,3 +4,10 @@ declare module "*.module.scss" {
   };
   export default classes;
 }
+
+declare module "*.module.css" {
+  const classes: {
+    readonly [key: string]: string;
+  };
+  export default classes;
+}
