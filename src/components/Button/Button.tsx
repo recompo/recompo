701ed5aee347bb002cc82f90294126b81d95b8bc
Gurpal Singh <gillgurpal699@gmail.com>
2022-03-20 16:30:51 +0530

    remove if statements from Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React, { FC, PropsWithChildren } from "react";
-import styles from "./Button.module.scss";
+import styles from "./Button.module.css";
 
 export type ButtonProps = {
   color?:
@@ -13,56 +13,15 @@ export type ButtonProps = {
 };
 
 const Button: FC<PropsWithChildren<ButtonProps>> = ({ children, color }) => {
-  // you can also take colors from themes
-  if (color === "primary")
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-primary"]}`}
-      >
-        {children}
-      </button>
-    );
-  else if (color === "secondary") {
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-secondary"]}`}
-      >
-        {children}
-      </button>
-    );
-  } else if (color === "success")
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-success"]}`}
-      >
-        {children}
-      </button>
-    );
-  else if (color === "error")
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-danger"]}`}
-      >
-        {children}
-      </button>
-    );
-  else if (color === "info")
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-info"]}`}
-      >
-        {children}
-      </button>
-    );
-  else if (color === "warning")
-    return (
-      <button
-        className={`${styles.Recompo_Button} ${styles["Recompo_Button-warning"]}`}
-      >
-        {children}
-      </button>
-    );
-  // we should add an else here which returns theme provided button
+  return (
+    <button
+      className={`${styles.Recompo_Button} ${
+        styles[`Recompo_Button-${color}`]
+      }`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
