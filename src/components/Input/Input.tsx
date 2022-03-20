fff9beec3c116e688bf1b7226df4c41eccb1576d
Gurpal Singh <gillgurpal699@gmail.com>
2022-03-20 17:29:26 +0530

    create input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from "react";
+import styles from "./Input.module.css";
+export type InputProps = {
+  label?: string;
+  disabled?: boolean;
+  borderRadius?: "xs" | "sm" | "md" | "lg" | "xl";
+  placeHolder?: string;
+  value?: string;
+  onChange?: any;
+  name: string;
+};
+
+const Input: FC<InputProps> = ({
+  label,
+  disabled,
+  onChange,
+  placeHolder,
+  value,
+  borderRadius,
+  name,
+}) => {
+  if (label) {
+    return (
+      <>
+        <label htmlFor={name}>
+          {label}
+          <input
+            name={name}
+            disabled={disabled}
+            onChange={onChange ? onChange : () => {}}
+            value={value}
+            placeholder={placeHolder}
+            className={`${styles[`Recompo_Input-${borderRadius}`]} ${
+              styles["Recompo_Input"]
+            } ${disabled && styles["Recompo_Input-disabled"]} `}
+          />
+        </label>
+      </>
+    );
+  }
+  return (
+    <input
+      name={name}
+      disabled={disabled}
+      onChange={onChange ? onChange : () => {}}
+      value={value}
+      placeholder={placeHolder}
+      className={`${styles[`Recompo_Button-${borderRadius}`]}`}
+    />
+  );
+};
+
+export default Input;
