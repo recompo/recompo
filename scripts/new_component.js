6dc03dd9e3df6ed7f18ed7de3d861677475d77e7
MarkosTh09 <markos.theocharis2009@gmail.com>
2022-03-21 17:53:07 +0200

    Fixed bug in new_component

--- scripts/new_component.js
+++ scripts/new_component.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 const fs = require("fs");
-const name = process.argv[1];
+
+const name = process.argv[0].match("node") ? process.argv[2] : process.argv[1];
 
 const index = `export { default } from './${name}'`;
 
 const component = `
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC } from 'react';
 import styles from './${name}.module.css'
 
 
@@ -17,7 +18,7 @@ export default ${name}
 `;
 const dir = `src/components/${name}`;
 
-fs.mkdir(`${dir}`);
-fs.writeFile(`${dir}/${name}.tsx`, component);
-fs.writeFile(`${dir}/index.ts`, index);
-fs.writeFile(`${dir}/${name}.module.css`, "");
+fs.mkdirSync(`${dir}`);
+fs.writeFileSync(`${dir}/${name}.tsx`, component);
+fs.writeFileSync(`${dir}/index.ts`, index);
+fs.writeFileSync(`${dir}/${name}.module.css`, "");
