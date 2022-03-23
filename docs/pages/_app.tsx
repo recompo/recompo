6c0f4f4c381798d08369ffc95373fa78087cb37e
MarkosTh09 <markos.theocharis2009@gmail.com>
2022-03-23 17:04:59 +0200

    Added docs and fixed issues with next.js in webpack

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from "next/app"
-import "nextra-theme-docs/style.css"
+import Prism from "prism-react-renderer/prism"
+(typeof global !== "undefined" ? global : window).Prism = Prism
 
+import "nextra-theme-docs/style.css"
+import "../../dist/style.css"
+import "prism-themes/themes/prism-one-dark.css"
 function Nextra({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
